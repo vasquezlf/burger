e52790341906acee7d784b87bc7d7cac7eeed143
Lorenzo Vasquez <vasquez.lf@gmail.com>
2018-06-04 19:37:14 -0700

    Stuff 2

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 var connection;
 
-if (pocess.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(pocess.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
